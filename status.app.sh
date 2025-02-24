fbf8d7af7eca8856034e0ee5d07305e484638ab1
lucian h. <lucianhanga@gmx.net>
2025-02-24 19:59:22 +0100

    update the mac os x file

---
+++ status.app.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --profile-directory="NewProfile" --app="https://adriansemafor123.z6.web.core.windows.net/" --new-window --window-size=400,800
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --profile-directory="NewProfile" --app="https://adriansemafor123.z6.web.core.windows.net/" --new-window
+exit
